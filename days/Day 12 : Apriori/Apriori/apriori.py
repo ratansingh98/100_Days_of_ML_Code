a185c89591eb0e343ff2ec8c4de2259250d265d0
Ratan Singh <ratansingh@pop-os.localdomain>
2018-07-18 10:11:22 +0530

    Day 12 Completed

---
+++ days/Day 12 : Apriori/Apriori/apriori.py
@@ -0,0 +1,24 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pprint
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
+pprint.pprint(rules) 
+for val in results:
+	pprint.pprint(str(val))
+	pprint.pprint("\n")	
